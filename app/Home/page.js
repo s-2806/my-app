39dd158539d2c122c0e7157bf5cfbc2aa5bbac44
s-2806 <susrithat@gmail.com>
2024-05-04 23:00:04 +0530

    first commit

--- app/Home/page.js
+++ app/Home/page.js
@@ -3,6 +3,31 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>this is called Home</h1>
+     <div>
+    <div id="Navbar">
+      <div id="Navbar-Left">
+        <Image src="/next.svg" alt="logo" width={50} height={50} />
+      </div>
+      <div id="Navbar-Right">
+        <a href="/Signup">Sign-up</a>
+        <a href="/Signin">Sign-in</a>
+      </div>
+   </div>
+    <div id="Container1">
+      <div id="Container1-Left">
+        <h1>Next.js</h1>
+        <p>Build your next project with Next.js</p>
+        <button>Get Started</button>
+      </div>
+      <div id="Container1-Right">
+        <Image src="/next.svg" alt="logo" width={400} height={400} />
+      </div>
+    </div>
+    <div id="Container2">
+      <div><a href="/"  >1</a></div>
+      <div><a href="/" >2</a></div>
+      <div><a href="/">3</a></div>
+      </div>
+    </div>
   );
 }
--- app/Signin/page.js
+++ app/Signin/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>this is called signin</h1>
+    <div>Sign-in</div>
+
   );
 }
