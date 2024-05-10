b7ce1bbcaa2091f304d115a08d1f1368be2e9b19
s-2806 <susrithat@gmail.com>
2024-05-10 08:34:41 +0530

    second commit

--- app/Home/page.js
+++ app/Home/page.js
@@ -6,27 +6,28 @@ export default function Home() {
      <div>
     <div id="Navbar">
       <div id="Navbar-Left">
-        <Image src="/next.svg" alt="logo" width={50} height={50} />
+        <Image src="/logo2.jpg" alt="logo" width={70} height={70} />
       </div>
       <div id="Navbar-Right">
+        <a href="/">Blogs</a>
         <a href="/Signup">Sign-up</a>
         <a href="/Signin">Sign-in</a>
       </div>
    </div>
     <div id="Container1">
       <div id="Container1-Left">
-        <h1>Next.js</h1>
-        <p>Build your next project with Next.js</p>
-        <button>Get Started</button>
+        <h1>Cooking success</h1>
+        <p>Build your cooking skills siiting at home 😀</p>
+        
       </div>
       <div id="Container1-Right">
-        <Image src="/next.svg" alt="logo" width={400} height={400} />
+        <Image src="/Best-Cutting-Board.jpg" alt="logo" width={700} height={400} />
       </div>
     </div>
     <div id="Container2">
-      <div><a href="/"  >1</a></div>
-      <div><a href="/" >2</a></div>
-      <div><a href="/">3</a></div>
+      <div><a href="/"  >recipe 1</a></div>
+      <div><a href="/" >recipe 2</a></div>
+      <div><a href="/">recipe 3</a></div>
       </div>
     </div>
   );
