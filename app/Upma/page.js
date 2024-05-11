b9cd0d1e40fcc60d8d6befd0b043c1d65dfe6288
s-2806 <susrithat@gmail.com>
2024-05-11 21:31:32 +0530

    sixth commit

--- app/Upma/page.js
+++ app/Upma/page.js
@@ -1,19 +1,32 @@
 import Image from "next/image";
-import styles from "./pageupma.module.css";
+
 export default function Home() {
   return (
+         <div> <div id="Navbar">
+        <div id="Navbar-Left">
+          <Image src="/logom.jpg" alt="logo" width={50} height={50} />
+          <span>Cooking success</span>
+        </div>
+        <div id="Navbar-Right">
+          <a href="/">Blogs</a>
+          <a href="/Signup">Sign-up</a>
+          <a href="/Signin">Sign-in</a>
+        </div>
+      </div>
     <div id="container_upma">
-            <div id="incontainer_upma1"><Image src="/upma.jpg" alt="logo" width={500} /></div>
+    <div id="incontainer_upma1"><Image src="/upma.jpg" alt="logo" width={500} height={300} /></div>
             <div id="incontainer_upma2">
                     <h1>Upma-Recipe</h1>
                     <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>lorum5
+                          
+                        </li>
                         <li></li>
                         <li></li>
                     </ul>
             </div>
-    </div>
+    </div></div>
       );
 }
